43c55570fbf503dcd527acbd4a9c7c650a313634
kayleerudaina <kayleerudaina@gmail.com>
2022-09-15 00:33:35 +0700

    edit on views

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+# TODO: Create views.
+def katalog_view (request):
+    katalog_item = CatalogItem.objects.all()
+    context = {
+    'list_item': katalog_item,
+    'Nama': 'Kaylee Rudaina Danisha',
+    'ID': '2106654971'
+    }
+    return render(request, "katalog.html", context)
