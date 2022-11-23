ac925e04a005ff30edab885ef07389a28a108f40
kayleerudaina <kayleerudaina@gmail.com>
2022-11-24 01:00:48 +0700

    add url for json

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from mywatchlist.views import watchlist_view
 from mywatchlist.views import show_json
+from mywatchlist.views import show_json_by_id
 from mywatchlist.views import show_xml
 
 # TODO: Implement Routings Here
@@ -11,4 +12,5 @@ urlpatterns = [
     path('html/', watchlist_view, name='watchlist_view'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), 
 ]
